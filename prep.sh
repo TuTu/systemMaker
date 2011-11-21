07b95cf4c69a14b0d76a2fb3f00df297ee34c3e4
KM Tu <tutu522@gmail.com>
2011-11-22 00:45:35 +0900

    Rename some file names to make them more reasonable.

--- prep.sh
+++ prep.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-python3.2 duplicateMol.py dmu3_single.gro ini.gro $1
+python3.2 makeMol.py -i dmu3_single.gro -o ini.gro -n $1
 editconf_d -f ini.gro -o ini.gro 
 
 python3.2 makeDest.py dest.xyz $1 -d $2
 
-vmd -dispdev text -e make.tcl 
+vmd -dispdev text -e arrange.tcl 
 
