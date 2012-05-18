5f388386f8f9c36ed6f3c1f791a0d0e1f8bd533e
KM Tu <tutu522@gmail.com>
2012-05-18 18:06:54 +0900

    Fix a bug in prep.sh, which creats an unused tmp.gro

--- prep.sh
+++ prep.sh
@@ -2,7 +2,7 @@
 # $1 = number of grid points, $2 = grid size in AA
 
 python3.2 makeMol.py -i ini.gro -o tmp.gro -n $1
-editconf_d -f tmp.gro -o tmp.gro 
+editconf_d -f tmp.gro -o ini.gro 
 
 python3.2 makeDest.py dest.xyz $1 -d $2
 
